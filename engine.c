e33dfdaf4905d2f2b9b7bae0f477ab75756c2ea1
Nankurunaisa99 <speranzabiagio99@gmail.com>
2024-05-21 22:48:02 +0200

    Added functions for generating pawn, knight, and king attack masks

--- engine.c
+++ engine.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include "define.h"
 
-
 //---------- MAIN ----------//
 
 int main(){
     U64 bitboard = 0ULL; 
-    print_bitboard(bitboard);
+
+    print_bitboard(mask_king_attacks(h8));
+
     return 0;
 }
--- engine_functions.c
+++ engine_functions.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 #include "define.h"
 
+//---------- GLOBALS ----------//
+
+const U64 not_a_file = 18374403900871474942ULL;
+const U64 not_h_file = 9187201950435737471ULL;
+const U64 not_hg_file = 4557430888798830399ULL;
+const U64 not_ab_file = 1822972355519532159;
+U64 pawn_attacks[2][64];
+U64 knight_attacks[64];
+U64 king_attacks[64];
+
+//---------- FUNCTIONS ----------//
+
+
 void print_bitboard(U64 bitboard){
     printf("\n");
 
@@ -16,5 +29,65 @@ void print_bitboard(U64 bitboard){
     }
 
     printf("\n     A  B  C  D  E  F  G  H\n\n");
-    printf("     Bitboard: %llX\n\n", bitboard);
+    printf("     Bitboard: %llud\n\n", bitboard);
+}
+
+U64 mask_pawn_attacks(int side, int square){
+
+    U64 attacks = 0ULL;
+    U64 bitboard = 0ULL;
+
+    set_bit(bitboard, square);
+    
+    // white pawns
+    if (!side)
+    {
+        if ((bitboard >> 7) & not_a_file) attacks |= (bitboard >> 7);
+        if ((bitboard >> 9) & not_h_file) attacks |= (bitboard >> 9);
+    }
+    
+    // black pawns
+    else
+    {
+        if ((bitboard << 7) & not_h_file) attacks |= (bitboard << 7);
+        if ((bitboard << 9) & not_a_file) attacks |= (bitboard << 9);    
+    }
+
+    return attacks;
+}
+
+U64 mask_knight_attacks(int square){
+    U64 attacks = 0ULL;
+    U64 bitboard = 0ULL;
+
+    set_bit(bitboard, square);
+
+    if ((bitboard >> 17) & not_h_file) attacks |= (bitboard >> 17);
+    if ((bitboard >> 15) & not_a_file) attacks |= (bitboard >> 15);
+    if ((bitboard >> 10) & not_ab_file) attacks |= (bitboard >> 10);
+    if ((bitboard >> 6) & not_h_file) attacks |= (bitboard >> 6);
+    if ((bitboard << 17) & not_a_file) attacks |= (bitboard << 17);
+    if ((bitboard << 15) & not_h_file) attacks |= (bitboard << 15);
+    if ((bitboard << 10) & not_hg_file) attacks |= (bitboard << 10);
+    if ((bitboard << 6) & not_a_file) attacks |= (bitboard << 6);
+
+    return attacks;
+}
+
+U64 mask_king_attacks(int square){
+    U64 attacks = 0ULL;
+    U64 bitboard = 0ULL;
+
+    set_bit(bitboard, square);
+
+    if ((bitboard >> 9) & not_h_file) attacks |= (bitboard >> 9);
+    if ((bitboard >> 7) & not_a_file) attacks |= (bitboard >> 7);
+    if ((bitboard >> 1) & not_h_file) attacks |= (bitboard >> 1);
+    if ((bitboard << 9) & not_a_file) attacks |= (bitboard << 9);
+    if ((bitboard << 7) & not_h_file) attacks |= (bitboard << 7);
+    if ((bitboard << 1) & not_a_file) attacks |= (bitboard << 1);
+    if ((bitboard >> 8)) attacks |= (bitboard >> 8);
+    if ((bitboard << 8)) attacks |= (bitboard << 8);
+
+    return attacks;
 }
